7d635b0a069f32db3558c9d2913d739db0c3a779
Daniel Ruoso <daniel@ruoso.com>
2024-05-26 11:47:51 -0400

    introduce FunctionCallForeach operator

--- src/networkprotocoldsl/operation.hpp
+++ src/networkprotocoldsl/operation.hpp
@@ -7,6 +7,7 @@
 #include <networkprotocoldsl/operation/dynamiclist.hpp>
 #include <networkprotocoldsl/operation/eq.hpp>
 #include <networkprotocoldsl/operation/functioncall.hpp>
+#include <networkprotocoldsl/operation/functioncallforeach.hpp>
 #include <networkprotocoldsl/operation/generatelist.hpp>
 #include <networkprotocoldsl/operation/if.hpp>
 #include <networkprotocoldsl/operation/int32literal.hpp>
@@ -41,13 +42,14 @@ namespace networkprotocoldsl {
  */
 using Operation = std::variant<
     operation::Add, operation::DynamicList, operation::Eq,
-    operation::FunctionCall, operation::GenerateList, operation::If,
-    operation::Int32Literal, operation::IntToAscii, operation::LesserEqual,
-    operation::LexicalPadGet, operation::LexicalPadInitialize,
-    operation::LexicalPadInitializeGlobal, operation::LexicalPadSet,
-    operation::Multiply, operation::OpSequence, operation::ReadInt32Native,
-    operation::ReadIntFromAscii, operation::ReadOctetsUntilTerminator,
-    operation::ReadStaticOctets, operation::StaticCallable, operation::Subtract,
+    operation::FunctionCall, operation::FunctionCallForEach,
+    operation::GenerateList, operation::If, operation::Int32Literal,
+    operation::IntToAscii, operation::LesserEqual, operation::LexicalPadGet,
+    operation::LexicalPadInitialize, operation::LexicalPadInitializeGlobal,
+    operation::LexicalPadSet, operation::Multiply, operation::OpSequence,
+    operation::ReadInt32Native, operation::ReadIntFromAscii,
+    operation::ReadOctetsUntilTerminator, operation::ReadStaticOctets,
+    operation::StaticCallable, operation::Subtract,
     operation::TerminateListIfReadAhead, operation::UnaryCallback,
     operation::WriteInt32Native, operation::WriteOctets,
     operation::WriteStaticOctets>;
---
+++ src/networkprotocoldsl/operation/functioncallforeach.cpp
@@ -0,0 +1,85 @@
+#include <networkprotocoldsl/operation/functioncallforeach.hpp>
+#include <networkprotocoldsl/operationconcepts.hpp>
+#include <networkprotocoldsl/value.hpp>
+
+#include <cassert>
+#include <type_traits>
+#include <variant>
+
+namespace networkprotocoldsl::operation {
+
+static OperationResult _function_call_for_each(ControlFlowOperationContext &ctx,
+                                               value::Callable callable,
+                                               value::DynamicList arglist) {
+  ctx.callable = callable;
+  ctx.arglist = arglist.values;
+  return ReasonForBlockedOperation::WaitingForCallableInvocation;
+}
+
+static OperationResult _function_call_for_each(ControlFlowOperationContext &ctx,
+                                               value::Callable callable,
+                                               Value arglist) {
+  return std::visit(
+      [&ctx, &callable](auto arglist_visited) {
+        return _function_call_for_each(ctx, callable, arglist_visited);
+      },
+      arglist);
+}
+
+static OperationResult _function_call_for_each(ControlFlowOperationContext &ctx,
+                                               auto a, auto b) {
+  return value::RuntimeError::TypeError;
+}
+
+OperationResult
+FunctionCallForEach::operator()(ControlFlowOperationContext &ctx,
+                                Arguments a) const {
+  if (ctx.callable.has_value()) {
+    if (ctx.callable_invoked) {
+      if (ctx.value.has_value()) {
+        ctx.accumulator->push_back(ctx.value.value());
+        if (ctx.accumulator->size() < ctx.arglist->size()) {
+          ctx.value.reset();
+          ctx.callable_invoked = false;
+          return ReasonForBlockedOperation::WaitingForCallableInvocation;
+        } else {
+          return value::DynamicList{ctx.accumulator};
+        }
+      } else {
+        return ReasonForBlockedOperation::WaitingForCallableResult;
+      }
+    } else {
+      return ReasonForBlockedOperation::WaitingForCallableInvocation;
+    }
+  } else {
+    return std::visit(
+        [&ctx, &a](auto &callable) {
+          return _function_call_for_each(ctx, callable, std::get<1>(a));
+        },
+        std::get<0>(a));
+  }
+}
+
+Value FunctionCallForEach::get_callable(
+    ControlFlowOperationContext &ctx) const {
+  assert(ctx.callable.has_value());
+  return ctx.callable.value();
+}
+void FunctionCallForEach::set_callable_invoked(
+    ControlFlowOperationContext &ctx) const {
+  ctx.callable_invoked = true;
+}
+void FunctionCallForEach::set_callable_return(ControlFlowOperationContext &ctx,
+                                              Value v) const {
+  ctx.value = v;
+}
+
+std::shared_ptr<const std::vector<Value>>
+FunctionCallForEach::get_argument_list(ControlFlowOperationContext &ctx) const {
+  // This presumes that this is a list of lists. A different operation would be
+  // needed in order to support cases where that's not going to be true.
+  return std::get<value::DynamicList>(ctx.arglist->at(ctx.accumulator->size()))
+      .values;
+}
+
+} // namespace networkprotocoldsl::operation
