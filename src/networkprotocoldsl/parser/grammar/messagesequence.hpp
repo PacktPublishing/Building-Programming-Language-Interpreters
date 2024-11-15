6f1886d4ac44094906d8b940e379bde909b9b542
Daniel Ruoso <daniel@ruoso.com>
2024-11-14 21:54:17 -0500

    Support for message sequence, including for loop

---
+++ src/networkprotocoldsl/parser/grammar/messagesequence.hpp
@@ -0,0 +1,95 @@
+#ifndef INCLUDED_NETWORKPROTOCOLDSL_PARSER_GRAMMAR_MESSAGEFORLOOP_HPP
+#define INCLUDED_NETWORKPROTOCOLDSL_PARSER_GRAMMAR_MESSAGEFORLOOP_HPP
+
+#include <memory>
+#include <vector>
+
+#include <networkprotocoldsl/lexer/token.hpp>
+#include <networkprotocoldsl/parser/grammar/identifier.hpp>
+#include <networkprotocoldsl/parser/grammar/tokenparts.hpp>
+#include <networkprotocoldsl/parser/grammar/traits.hpp>
+#include <networkprotocoldsl/parser/support/recursiveparser.hpp>
+#include <networkprotocoldsl/parser/tree/messageforloop.hpp>
+
+namespace networkprotocoldsl::parser::grammar {
+
+class MessageSequence; // Forward declaration
+
+class MessageForLoop
+    : public support::RecursiveParser<MessageForLoop, ParseTraits,
+                                      Tracer<MessageForLoop>> {
+public:
+  static constexpr const char *name = "MessageForLoop";
+  static void partial_match() {}
+  static IdentifierReference *recurse_one(lexer::token::keyword::For) {
+    return nullptr;
+  }
+  static void partial_match(lexer::token::keyword::For,
+                            std::shared_ptr<const tree::IdentifierReference>) {}
+  static IdentifierReference *
+  recurse_one(lexer::token::keyword::For,
+              std::shared_ptr<const tree::IdentifierReference>,
+              lexer::token::keyword::In) {
+    return nullptr;
+  }
+  static void partial_match(lexer::token::keyword::For,
+                            std::shared_ptr<const tree::IdentifierReference>,
+                            lexer::token::keyword::In,
+                            std::shared_ptr<const tree::IdentifierReference>) {}
+  static MessageSequence *
+  recurse_one(lexer::token::keyword::For,
+              std::shared_ptr<const tree::IdentifierReference>,
+              lexer::token::keyword::In,
+              std::shared_ptr<const tree::IdentifierReference>,
+              lexer::token::punctuation::CurlyBraceOpen) {
+    return nullptr;
+  }
+  static void partial_match(lexer::token::keyword::For,
+                            std::shared_ptr<const tree::IdentifierReference>,
+                            lexer::token::keyword::In,
+                            std::shared_ptr<const tree::IdentifierReference>,
+                            lexer::token::punctuation::CurlyBraceOpen,
+                            std::shared_ptr<const tree::MessageSequence>) {}
+  static ParseStateReturn
+  match(TokenIterator begin, TokenIterator end, lexer::token::keyword::For,
+        std::shared_ptr<const tree::IdentifierReference> identifier1,
+        lexer::token::keyword::In,
+        std::shared_ptr<const tree::IdentifierReference> identifier2,
+        lexer::token::punctuation::CurlyBraceOpen,
+        std::shared_ptr<const tree::MessageSequence> seq,
+        lexer::token::punctuation::CurlyBraceClose) {
+    return {std::make_shared<const tree::MessageForLoop>(identifier1,
+                                                         identifier2, seq),
+            begin, end};
+  }
+};
+
+class MessagePart : public support::RecursiveParser<MessagePart, ParseTraits,
+                                                    Tracer<MessagePart>> {
+public:
+  static constexpr const char *name = "MessagePart";
+  static std::tuple<TokenSequence, Terminator, MessageForLoop> *recurse_any() {
+    return nullptr;
+  }
+  static ParseStateReturn match(TokenIterator begin, TokenIterator end,
+                                tree::MessagePart part) {
+    return {std::make_shared<const tree::MessagePart>(part), begin, end};
+  }
+};
+
+class MessageSequence
+    : public support::RecursiveParser<MessageSequence, ParseTraits,
+                                      Tracer<MessageSequence>> {
+public:
+  static constexpr const char *name = "MessageSequence";
+  static MessagePart *recurse_many() { return nullptr; }
+  static ParseStateReturn
+  match(TokenIterator begin, TokenIterator end,
+        std::vector<std::shared_ptr<const tree::MessagePart>> parts) {
+    return {std::make_shared<const tree::MessageSequence>(parts), begin, end};
+  }
+};
+
+} // namespace networkprotocoldsl::parser::grammar
+
+#endif // INCLUDED_NETWORKPROTOCOLDSL_PARSER_GRAMMAR_MESSAGEFORLOOP_HPP
---
+++ tests/021-grammar-messagesequence.cpp
@@ -0,0 +1,71 @@
+#include <networkprotocoldsl/lexer/token.hpp>
+#include <networkprotocoldsl/lexer/tokenize.hpp>
+#include <networkprotocoldsl/parser/grammar/messagesequence.hpp>
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <vector>
+
+using namespace networkprotocoldsl;
+
+TEST(MessageSequenceTest, SingleMessagePartMatch) {
+  auto maybe_tokens = lexer::tokenize("tokens { \"stringLiteral\" }");
+  ASSERT_TRUE(maybe_tokens.has_value());
+  std::vector<lexer::Token> &tokens = maybe_tokens.value();
+  auto result =
+      parser::grammar::MessagePart::parse(tokens.cbegin(), tokens.cend());
+  ASSERT_EQ(result.begin, tokens.cend());
+  ASSERT_TRUE(result.node.has_value());
+}
+
+TEST(MessageSequenceTest, SingleMessageSequenceMatch) {
+  auto maybe_tokens = lexer::tokenize("tokens { \"stringLiteral\" }");
+  ASSERT_TRUE(maybe_tokens.has_value());
+  std::vector<lexer::Token> &tokens = maybe_tokens.value();
+  auto result =
+      parser::grammar::MessageSequence::parse(tokens.cbegin(), tokens.cend());
+  ASSERT_EQ(result.begin, tokens.cend());
+  ASSERT_TRUE(result.node.has_value());
+}
+
+TEST(MessageSequenceTest, SingleMessageSequenceMatch2) {
+  auto maybe_tokens = lexer::tokenize("terminator { \"stringLiteral\" }");
+  ASSERT_TRUE(maybe_tokens.has_value());
+  std::vector<lexer::Token> &tokens = maybe_tokens.value();
+  auto result =
+      parser::grammar::MessageSequence::parse(tokens.cbegin(), tokens.cend());
+  ASSERT_EQ(result.begin, tokens.cend());
+  ASSERT_TRUE(result.node.has_value());
+}
+
+TEST(MessageSequenceTest, MessageFor) {
+  auto maybe_tokens =
+      lexer::tokenize("for i in list { tokens { \"stringLiteral\" } }");
+  ASSERT_TRUE(maybe_tokens.has_value());
+  std::vector<lexer::Token> &tokens = maybe_tokens.value();
+  auto result =
+      parser::grammar::MessageForLoop::parse(tokens.cbegin(), tokens.cend());
+  ASSERT_EQ(result.begin, tokens.cend());
+  ASSERT_TRUE(result.node.has_value());
+}
+
+TEST(MessageSequenceTest, LongMessageSequenceMatch) {
+  auto maybe_tokens =
+      lexer::tokenize("    tokens { verb } "
+                      "    terminator { \" \" } "
+                      "    tokens { request_target } "
+                      "    terminator { \" \" } "
+                      "    tokens { "
+                      "        \"HTTP/\" major_version \".\" minor_version "
+                      "    } "
+                      "    terminator { \"\\r\\n\" } ");
+  ASSERT_TRUE(maybe_tokens.has_value());
+  std::vector<lexer::Token> &tokens = maybe_tokens.value();
+  auto result =
+      parser::grammar::MessageSequence::parse(tokens.cbegin(), tokens.cend());
+  ASSERT_EQ(result.begin, tokens.cend());
+  ASSERT_TRUE(result.node.has_value());
+  auto seq = std::get<std::shared_ptr<const parser::tree::MessageSequence>>(
+      result.node.value());
+  ASSERT_EQ(6, seq->size());
+}
