2cb66426f3f0abd883b09dbbaf85612b199bdf8c
Daniel Ruoso <daniel@ruoso.com>
2025-01-20 12:58:55 -0500

    new constructors for value to make it easier to use

--- src/networkprotocoldsl/value.hpp
+++ src/networkprotocoldsl/value.hpp
@@ -46,10 +46,29 @@ struct Callable {
 
 struct DynamicList {
   std::shared_ptr<const std::vector<Value>> values;
+  DynamicList() = default;
+  explicit DynamicList(std::vector<Value> &&vals)
+      : values(std::make_shared<const std::vector<Value>>(std::move(vals))) {}
+  explicit DynamicList(std::initializer_list<Value> init)
+      : values(std::make_shared<const std::vector<Value>>(init)) {}
+  explicit DynamicList(const std::vector<Value> &vals)
+      : values(std::make_shared<const std::vector<Value>>(vals)) {}
+  explicit DynamicList(std::shared_ptr<const std::vector<Value>> vals)
+      : values(vals) {}
+  explicit DynamicList(std::shared_ptr<std::vector<Value>> vals)
+      : values(std::const_pointer_cast<const std::vector<Value>>(vals)) {}
 };
 
 struct Octets {
   std::shared_ptr<const std::string> data;
+  Octets() = default;
+  explicit Octets(std::string &&d)
+      : data(std::make_shared<const std::string>(std::move(d))) {}
+  explicit Octets(const char *d)
+      : data(std::make_shared<const std::string>(d)) {}
+  explicit Octets(const std::string &d)
+      : data(std::make_shared<const std::string>(d)) {}
+  explicit Octets(std::shared_ptr<const std::string> d) : data(d) {}
 };
 
 } // namespace value
